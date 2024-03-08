13fde58d51e6f349ebac9c6eb4bc580abd78f57e
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-08 10:00:07 +0500

    Add files via upload
    Q#04 of 45Question Assignment

---
+++ Qno4.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote: Finde a quote from a famous person you admire. Print the quote and the name its author. Your output look somthing like the folloing, including the Quotation marks:
+// AlbertEinstein once said,"A person who never made a mistake never tried anything new."  
+let name1 = ("allama Iqbal said");
+console.log(`"${name1},"Nations are born in the hearts of poets, they prosper and die in the hands of politicians."`);
---
+++ Qno4.ts
@@ -0,0 +1,6 @@
+// Famous Quote: Finde a quote from a famous person you admire. Print the quote and the name its author. Your output look somthing like the folloing, including the Quotation marks:
+
+// AlbertEinstein once said,"A person who never made a mistake never tried anything new."  
+
+let name1: string=("allama Iqbal said") 
+console.log(`"${name1},"Nations are born in the hearts of poets, they prosper and die in the hands of politicians."`)
